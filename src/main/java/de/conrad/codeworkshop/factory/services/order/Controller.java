3675fe2466d2ef18e9d0dd2d84fc5b9519fddd0c
zakaria <zakariaouasbir@gmail.com>
2022-12-11 16:32:21 +0100

    Task 2 : Adjust de.conrad.codeworkshop.factory.services.order.Controller so that it accepts and produces JSON.

--- src/main/java/de/conrad/codeworkshop/factory/services/order/Controller.java
+++ src/main/java/de/conrad/codeworkshop/factory/services/order/Controller.java
@@ -2,27 +2,40 @@ package de.conrad.codeworkshop.factory.services.order;
 
 import de.conrad.codeworkshop.factory.services.order.api.Order;
 import de.conrad.codeworkshop.factory.services.order.api.OrderConfirmation;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
 
 /**
  * @author Andreas Hartmann
  */
 @RestController("orderController")
-@RequestMapping("/order")
+@RequestMapping(
+				value = "/order",
+				consumes = MediaType.APPLICATION_JSON_VALUE,
+				produces = MediaType.APPLICATION_JSON_VALUE
+				)
 public class Controller {
 
     private final Service factoryService;
 
-    @Autowired
+    @Autowired	
     public Controller(final Service service) {
         this.factoryService = service;
     }
 
     @PostMapping("/create")
-    public OrderConfirmation createOrder(final Order order) {
-        return factoryService.createOrder(order);
-    }
+    public ResponseEntity<OrderConfirmation> createOrder(@Valid @RequestBody final Order order) {
+    	OrderConfirmation orderConfirmation =  factoryService.createOrder(order);
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderConfirmation);
+        }
 }
--- src/main/java/de/conrad/codeworkshop/factory/services/order/api/OrderNumber.java
+++ src/main/java/de/conrad/codeworkshop/factory/services/order/api/OrderNumber.java
@@ -9,17 +9,17 @@ import static java.math.BigInteger.valueOf;
  * @author Andreas Hartmann
  */
 public class OrderNumber {
-    private final BigInteger orderNumberPlain;
+	private final BigInteger orderNumberPlain;
 
-    public OrderNumber(BigInteger futureOrderNumberPlain) {
-        orderNumberPlain = futureOrderNumberPlain;
-    }
+	public OrderNumber(BigInteger futureOrderNumberPlain) {
+		orderNumberPlain = futureOrderNumberPlain;
+	}
 
-    public static OrderNumber generate() {
-        return new OrderNumber(valueOf(new Random().nextInt()));
-    }
+	public static OrderNumber generate() {
+		return new OrderNumber(valueOf(new Random().nextInt()));
+	}
 
-    public BigInteger getOrderNumberPlain() {
-        return orderNumberPlain;
-    }
+	public BigInteger getOrderNumberPlain() {
+		return orderNumberPlain;
+	}
 }
