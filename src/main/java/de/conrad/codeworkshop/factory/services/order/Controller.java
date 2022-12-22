ace7376bec26ba13438c86cb26cee20728fa244f
olyamin <olyamin@sinergo.ru>
2022-12-22 09:47:30 +0600

    Task 2 (accepts and produces JSON)

--- src/main/java/de/conrad/codeworkshop/factory/services/order/Controller.java
+++ src/main/java/de/conrad/codeworkshop/factory/services/order/Controller.java
@@ -3,6 +3,7 @@ package de.conrad.codeworkshop.factory.services.order;
 import de.conrad.codeworkshop.factory.services.order.api.Order;
 import de.conrad.codeworkshop.factory.services.order.api.OrderConfirmation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,9 @@ public class Controller {
         this.factoryService = service;
     }
 
-    @PostMapping("/create")
+    @PostMapping(value = "/create",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     public OrderConfirmation createOrder(final Order order) {
         return factoryService.createOrder(order);
     }
